91beb30bb66745b7c1d26f4386971c7dd456327e
TayssaTiesen <99990338+TayssaTiesen@users.noreply.github.com>
2022-03-25 15:09:58 -0300

    Add files via upload

---
+++ Python POO/Conta.py
@@ -0,0 +1,8 @@
+Class Conta:
+    def __init__ (self, numero, titular, saldo, limite):
+        print('Construindo um objeto ... {}'.format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+                                                                                                               
